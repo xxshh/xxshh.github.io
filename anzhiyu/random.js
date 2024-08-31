d65346aa5c6ab63c1a952d0824d8e203d3470fa9
xxshh <2215678543@qq.com>
2024-08-31 21:31:35 +0800

    Site updated: 2024-08-31 21:31:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/31/blog/my-blog/","2024/08/31/my-blog/","2024/08/31/test/","2024/08/31/Obsidian Vault/BUUCTF/BUUCTF Basic 解题记录/","2024/08/31/blog/欢迎/","2024/08/31/Obsidian Vault/app推广/Thegreenwhale下载教程/","2024/08/23/HELLO WORLD/"];function toRandomPost(){
+var posts=["2024/08/31/BUUCTF Basic 解题记录1/","2024/08/31/BUUCTF Basic 解题记录/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
