c6da1ec366abb129eb7fbf5e344c5a1e3b918130
xxshh <2215678543@qq.com>
2024-09-04 13:46:10 +0800

    Site updated: 2024-09-04 13:46:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/31/BUUCTF Basic 解题记录/"];function toRandomPost(){
+var posts=["2024/08/31/BUUCTF Basic 解题记录/","2024/09/02/爬虫：获取ins指定用户的粉丝数/","2024/09/03/linux命令/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
