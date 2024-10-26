849e16434dc07a5933b00a8a78f42574091946b3
xxshh <2215678543@qq.com>
2024-10-26 22:59:07 +0800

    Site updated: 2024-10-26 22:59:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/31/BUUCTF Basic 解题记录/","2024/09/03/linux命令/","2024/09/06/BUUCTF-Web-解题记录/","2024/09/02/爬虫：获取ins指定用户的粉丝数/"];function toRandomPost(){
+var posts=["2024/08/31/BUUCTF Basic 解题记录/","2024/09/03/linux命令/","2024/09/06/BUUCTF-Web-解题记录/","2024/09/02/爬虫：获取ins指定用户的粉丝数/","2024/10/18/笔记/","2024/10/25/NewStar-CTF-2024-Week1-Web/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
